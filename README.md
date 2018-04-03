d8bbca9e1371364d790d76081d8f7a64b83dd0c6
Jakub Stefanski <code@jakubstefanski.com>
2018-04-04 00:59:24 +0200

    Add main function with error handling and README

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# ethvalidator
-Validation of private key against address using message signing
+# EthValidator
+
+This console utility performs validation of private keys against its addresses.
+
+# Motivation
+
+Bug in JavaScript library ethereumjs-utils (< 2.2.3) used by many projects (for example MyEtherWallet)
+cost many people lost coins:
+
+https://www.reddit.com/r/ethereum/comments/47nkoi/psa_check_your_ethaddressorg_wallets_and_any/
+https://www.reddit.com/r/ethereum/comments/48rt6n/using_myetherwalletcom_just_burned_me_for/
+
+This program was created to defend against this kind of bugs.
+To prove correctness of the addresses it uses completely different cryptographic libraries (Java implementation instead of C libraries used by JavaScript libraries and many other technologies).
+
+# Usage
+
+Check `examples/*.json` files for correct input format. It is consistent with VanityEth.
+
+## Requirements
+
+- JRE 1.8
+
+## Examples
+
+```bash
+java -jar ethvalidator.jar examples/correct.json
+Processed 3 entries
+```
+
+```bash
+java -jar ethvalidator.jar examples/incorrect.json
+Invalid address, expected 0xc775FC9feadE71B9E39fAEE43e15e64461d7c206, instead got 0xdeadbeefeadE71B9E39fAEE43e15e64461d7c206
+Processed 3 entries
+```
+
+# Building
+
+## Requirements
+
+- JDK 1.8
+- Gradle 3 (or newer)
+
+## Steps
+
+### Building
+
+```bash
+gradle build
+```
+
+### Running tests
+
+```bash
+gradle test
+```
+
+### Creating fat JAR
+
+It creates JAR bundle as a single file at `./build/libs/ethvalidator-1.0-SNAPSHOT-all.jar`
+
+```bash
+gradle shadowJar
+```
--- src/main/java/io/photochain/Main.java
+++ src/main/java/io/photochain/Main.java
@@ -1,8 +1,46 @@
 package io.photochain;
 
+import com.fasterxml.jackson.core.JsonParseException;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        if (args.length != 1) {
+            System.err.println("Specify a single parameter with path to JSON entries file");
+            return;
+        }
+
+        try {
+            ValidateFile(args[0]);
+        } catch (FileNotFoundException e) {
+            System.err.println("File does not exist. Specify correct path as the first argument.");
+        } catch (JsonParseException e) {
+            System.err.println("Failed to parse JSON file: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Failed to read file: " + e.getMessage());
+        }
+    }
+
+    private static void ValidateFile(String arg) throws IOException {
+        EntryParser parser = new EntryParser();
+        AddressValidator validator = new AddressValidator();
+
+        FileInputStream stream = new FileInputStream(arg);
+        List<Entry> entries = parser.parse(stream);
+
+        for (Entry entry : entries) {
+            try {
+                validator.validate(entry);
+            } catch (InvalidAddressException e) {
+                System.err.println("Invalid address, expected " + e.getExpectedAddress() + ", instead got " + e.getActualAddress());
+            }
+        }
+
+        System.out.println("Processed " + entries.size() + " entries");
     }
 }
 
